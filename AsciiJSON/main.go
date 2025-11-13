1db29c61265ec28ce70d33f6cb4aea2991d3cc08
fengjutian <fjutian@foxmail.com>
2025-11-13 09:40:07 +0800

    feat: 添加AsciiJSON示例及依赖
    添加一个新的Gin示例，展示如何使用AsciiJSON方法将非ASCII字符转义为Unicode编码
    同时添加了必要的go.mod和go.sum文件以管理项目依赖

---
+++ AsciiJSON/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+
+		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	router.Run(":8080")
+}
